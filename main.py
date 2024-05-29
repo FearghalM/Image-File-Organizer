9e9b4ddda83e48457eda8851bd82367bddf79635
FearghalM <femcmorrow@gmail.com>
2024-05-29 20:17:36 +0100

    feat: Enable folder selection and display selected folder path

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import tkinter as tk
+from tkinter import filedialog
 
-
+def select_folder():
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        folder_label.config(text=folder_path)
+        organize_images_btn.config(state="normal")
 
 root = tk.Tk()
 root.title("Image Organizer")
 root.geometry("300x250")
 
-select_folder_btn = tk.Button(root, text="Select Folder")
+select_folder_btn = tk.Button(root, text="Select Folder", command=select_folder)
 select_folder_btn.pack(pady=20, padx=20)
 
 folder_label = tk.Label(root, text="", wraplength=400)
