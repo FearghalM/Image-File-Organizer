547cecc5d345c27cf43e8128e1b39e87545a0cca
FearghalM <femcmorrow@gmail.com>
2024-05-29 20:14:32 +0100

    chore: Update main.py with GUI improvements

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Image Organizer")
+root.geometry("300x250")
 
-tk.Button(root, text="Select Folder").pack(pady=20)
+select_folder_btn = tk.Button(root, text="Select Folder")
+select_folder_btn.pack(pady=20, padx=20)
+
 folder_label = tk.Label(root, text="", wraplength=400)
-folder_label.pack(pady=20, padx=20)
+folder_label.pack(pady=10, padx=10)
 
 organize_images_btn = tk.Button(root, text="Organize Images", state="disabled")
-organize_images_btn.pack(pady=20)
+organize_images_btn.pack(pady=20,padx=20)
 
 root.mainloop()
